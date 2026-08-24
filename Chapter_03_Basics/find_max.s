5a54958610ece7930ac1198bc71aa48c5d1a8899
ordinaryteen <nafimulyo914@gmail.com>
2026-08-24 17:10:31 +0700

    Project 2: Completed find_max.s with indexed addressing mode

---
+++ Chapter_03_Basics/find_max.s
@@ -0,0 +1,29 @@
+.section .data
+data_items:
+    .long 3, 12, 5, 35, 2, 53, 89, 64, 13, 18, 22, 6, 7, 23, 102, 0
+
+.section .text
+.globl _start
+
+_start:
+    movl $0, %edi
+
+    movl data_items(,%edi,4), %eax
+    movl %eax, %ebx
+
+iter:
+    cmpl $0, %eax
+    je done
+
+    incl %edi
+    movl data_items(,%edi,4), %eax
+
+    cmpl %ebx, %eax
+    jle iter
+
+    movl %eax, %ebx
+    jmp iter
+
+done:
+    movl $1, %eax
+    int $0x80
